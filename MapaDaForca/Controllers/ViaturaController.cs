3147acc0bef0e2b6bf6f2fcc93ae000e99a8af0c
Leandro Reis <leandro.reis.external@hexagonsi.com>
2019-03-26 19:11:48 +0100

    Created the controllers

---
+++ MapaDaForca/Controllers/ViaturaController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MapaDaForca.Controllers
+{
+    public class ViaturaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
