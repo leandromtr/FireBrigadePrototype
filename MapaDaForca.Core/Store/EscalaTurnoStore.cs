c028b6b9721d6e97e3e0910446a5b229d5d3ceb8
Leandro Reis <leandro.reis.external@hexagonsi.com>
2019-04-03 17:11:11 +0200

    Draft of EscalaTurno

---
+++ MapaDaForca.Core/Store/EscalaTurnoStore.cs
@@ -0,0 +1,50 @@
+﻿using MapaDaForca.Core.Store;
+using MapaDaForca.Data.Repository;
+using MapaDaForca.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MapaDaForca.Core.Store
+{
+    public class EscalaTurnoStore : IEscalaTurnoStore
+    {
+        private readonly IEscalaTurnoRepository _repository;
+
+        public EscalaTurnoStore(IEscalaTurnoRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public IList<EscalaTurno> GetAll()
+        {
+            return _repository.GetAll();
+        }
+
+        public EscalaTurno GetById(Guid id)
+        {
+            return _repository.GetById(id);
+        }
+
+        public EscalaTurno Save(EscalaTurno save)
+        {
+            EscalaTurno saved = null;
+
+            if (_repository.IsExisting(save.Id))
+            {
+                saved = _repository.Update(save);
+            }
+            else
+            {
+                saved = _repository.Create(save);
+            }
+
+            return saved;
+        }
+
+        public bool Delete(Guid id)
+        {
+            return _repository.Delete(id);
+        }
+    }
+}
---
+++ MapaDaForca.Core/Store/Interface/IEscalaTurnoStore.cs
@@ -0,0 +1,18 @@
+﻿using MapaDaForca.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MapaDaForca.Core.Store
+{
+    public interface IEscalaTurnoStore
+    {
+        IList<EscalaTurno> GetAll();
+
+        EscalaTurno GetById(Guid id);
+
+        EscalaTurno Save(EscalaTurno save);
+
+        bool Delete(Guid id);
+    }
+}
