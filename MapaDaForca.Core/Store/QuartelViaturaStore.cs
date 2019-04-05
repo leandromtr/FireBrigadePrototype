e59b933339d02341b88fa99e76f6b3ac7997f49f
leandromtr <leandromtr@hotmail.com>
2019-04-05 16:46:31 +0200

    QuartelViatura structure

---
+++ MapaDaForca.Core/Store/QuartelViaturaStore.cs
@@ -0,0 +1,56 @@
+﻿using MapaDaForca.Core.Store;
+using MapaDaForca.Data.Repository;
+using MapaDaForca.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MapaDaForca.Core.Store
+{
+    public class QuartelViaturaStore : IQuartelViaturaStore
+    {
+        private readonly IQuartelViaturaRepository _repository;
+
+        public QuartelViaturaStore(IQuartelViaturaRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public IList<QuartelViatura> GetAll()
+        {
+            return _repository.GetAll().ToList();
+        }
+
+        public IList<QuartelViatura> GetByQuartelId(Guid quartelId)
+        {
+            return _repository.GetByQuartelId(quartelId).ToList();
+        }
+
+        public QuartelViatura GetById(Guid id)
+        {
+            return _repository.GetById(id);
+        }
+
+        public QuartelViatura Save(QuartelViatura save)
+        {
+            QuartelViatura saved = null;
+
+            if (_repository.IsExisting(save.Id))
+            {
+                saved = _repository.Update(save);
+            }
+            else
+            {
+                saved = _repository.Create(save);
+            }
+
+            return saved;
+        }
+
+        public bool Delete(Guid id)
+        {
+            return _repository.Delete(id);
+        }
+    }
+}
---
+++ MapaDaForca/ViewModel/QuartelViewModel.cs
@@ -0,0 +1,16 @@
+﻿using MapaDaForca.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MapaDaForca.ViewModel
+{
+    public class QuartelViewModel
+    {
+        public Quartel Quartel { get; set; }
+
+        public List<QuartelViatura> QuartelViaturas { get; set; }
+        
+    }
+}
