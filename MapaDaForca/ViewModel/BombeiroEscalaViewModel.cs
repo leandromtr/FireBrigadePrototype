8c75ff482f555b2d0214b54abddcbd55ba448007
leandromtr <leandromtr@hotmail.com>
2019-05-27 11:54:11 +0200

    Page of EscalaAnual and EscalAnualFerias

--- MapaDaForca/ViewModel/BombeiroEscalaViewModel.cs
+++ MapaDaForca/ViewModel/BombeiroEscalaViewModel.cs
@@ -12,6 +12,8 @@ namespace MapaDaForca.ViewModel
 
         public Escala Escala { get; set; }
 
+        public List<Escala> Escalas { get; set; }
+
         public List<Funcao> Funcoes { get; set; }
 
         public List<Quartel> Quarteis { get; set; }
